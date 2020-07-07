9515ee42c3c99bd17c53a9771982de37579d5f8f
nadezdatsygankova <nadia_pritchina@mail.ru>
2020-07-07 14:17:21 -0400

    book Russian 1-13

---
+++ Book Russian/13/Task4/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+
+int doubleNumber(int n)
+{
+   return 2* n;
+}
+
+
+
+int main()
+{
+   int n = 5;
+
+   std::cout << "Please enter a number: ";
+   std::cin >> n;
+   std::cout << doubleNumber(n);
+   return 0;
+}
