83df612943894455eae9afdb7150673c0dd3499b
Nadezda Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2020-12-19 15:27:38 -0500

    Create SavingAccount.cpp

---
+++ AccountVector/SavingAccount.cpp
@@ -0,0 +1,39 @@
+#include "SavingAccount.h"
+#include<iostream>
+//#include "Account.h"
+
+
+SavingAccount::SavingAccount() :Account(), rate(0.0),AT(1)
+{
+
+}
+
+SavingAccount::SavingAccount(double newBalance, double newRate, int at):Account(newBalance,at), rate(newRate)
+{
+   if (at != 1)
+   {
+      AT = 1;
+   }
+}
+
+int SavingAccount::getAT()
+{
+   return AT; 
+}
+//set interest rate
+void SavingAccount::setRate(double newRate)
+{
+   rate = newRate;
+}
+double SavingAccount::getRate() const
+{
+   return rate;
+}
+
+//function that returns a double indicating the amount of interest earned by an account.
+double SavingAccount::calcInterest()
+{
+   double rate = getRate();
+  double  balance = getBalance();
+   return (rate * balance);
+}
