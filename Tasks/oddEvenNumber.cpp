b0cba5cd0925b2f881ebfc0c8eaac86943546c8f
Nadezda Tsygankova <47821694+nadezdatsygankova@users.noreply.github.com>
2020-12-19 21:40:21 -0500

    Update and rename Work2/Q9/Source.cpp to Tasks/oddEvenNumber.cpp

--- Tasks/oddEvenNumber.cpp
+++ Tasks/oddEvenNumber.cpp
@@ -1,10 +1,7 @@
-/*Author: Nadezda Tsygankova
-  Description: Workshop02/Q9
-  Due date:27/05/2020
-*/
+
 /*
 Write a program that reads an integer and determines and prints whether
-it’s odd or even. 
+itâ€™s odd or even. 
 [Hint: Use the modulus operator. An even number is a multiple of two.
 Any multiple of two leaves a remainder of zero when divided by 2.]
 */
